720c18df51be229e3cdc46e8f5e62469d4729d9d
knotfest79 <vikingsfortune@gmail.com>
2023-05-20 15:37:38 +1000

    hi

---
+++ ThreeJs/src/App.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
+
+const Container = styled.div`
+height: 100vh;
+  
+  
+  
+  justify-content: center;
+  align-items: center;
+
+ scroll-snap-type: y mandatory;
+ scroll-behavior: smooth;
+ overflow-y: auto;
+ scrollbar-width: none;
+ color: white;
+ background: url("./img/bg.jpeg");
+ background-size: cover;
+ background-position: center;
+
+ &::-webkit-scrollbar {
+  display: none;
+ }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
