e0f59cb8827245dff9782eb539bfeed5f4d56050
knotfest79 <vikingsfortune@gmail.com>
2023-05-21 13:30:01 +1000

    hi

--- ThreeJs/src/App.jsx
+++ ThreeJs/src/App.jsx
@@ -1,32 +1,33 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
 
 const Container = styled.div`
-height: 100vh;
-  
-  
-  
+  max-width: 1400px;
+  height: 100vh;
+  width: 100vw;
+
   justify-content: center;
   align-items: center;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  background-size: cover;
+  background-position: center;
 
- scroll-snap-type: y mandatory;
- scroll-behavior: smooth;
- overflow-y: auto;
- scrollbar-width: none;
- color: white;
- background: url("./img/bg.jpeg");
- background-size: cover;
- background-position: center;
-
- &::-webkit-scrollbar {
-  display: none;
- }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 function App() {
+  //Remove default margin from body element
+  document.body.style.margin = "0";
   return (
     <Container>
       <Hero />
